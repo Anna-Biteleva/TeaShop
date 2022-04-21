52fabd76b8552397d62194f60d0df664a92b7cb9
anbi <biteleva.anna@gmail.com>
2022-04-21 14:57:29 +0300

    Initial commit

---
+++ src/main/java/org/biteleva/App.java
@@ -0,0 +1,13 @@
+package org.biteleva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
