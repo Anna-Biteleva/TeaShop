7c4ee3ff4a28527e1f72c74b49813cc5d0c9c1b6
anbi <biteleva.anna@gmail.com>
2022-04-29 15:15:56 +0300

    ДЗ-3, 4 теста.

---
+++ src/test/java/org/biteleva/Lesson3/OolongTest.java
@@ -0,0 +1,42 @@
+package org.biteleva.Lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class OolongTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        driver.get("https://artoftea.ru");
+
+        WebElement webElement1 = driver.findElement(By.name("search"));
+        webElement1.click();
+        webElement1.sendKeys("ГАБА");
+        WebElement webElement2 = driver.findElement(By.cssSelector(".btn-default > .fa")); // поиск
+        webElement2.click();
+        WebElement webElement3 = driver.findElement(By.xpath("//button[@onclick=\"addCartOption('search','524', '1');\"]")); // кнопка "Купить"
+        webElement3.click();
+        WebElement webElement4 = driver.findElement(By.cssSelector(".fa-shopping-cart")); // иконка корзины
+        webElement4.click();
+        WebElement webElement6 = driver.findElement(By.cssSelector("a.btn.btn-primary.btn-block")); // выпадающее окно
+        webElement6.click();
+        WebElement webElement7 = driver.findElement(By.cssSelector(".fa-times-circle")); //очистить корзину
+        webElement7.click();
+
+        //driver.quit();
+
+
+
+    }
+}
---
+++ src/test/java/org/biteleva/Lesson3/VkTest.java
@@ -0,0 +1,32 @@
+package org.biteleva.Lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class VkTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        driver.get("https://artoftea.ru");
+        WebElement webElement1 = driver.findElement(By.cssSelector(".topbar_inner .socsetii-vk > .fa"));
+        webElement1.click();
+
+       // WebElement webElement2 = driver.findElement(By.linkText("https://artoftea.ru"));
+        WebElement webElement2 = driver.findElement(By.xpath("//*[@id=\"page_info_wrap\"]/div/div/div[2]/div/a"));
+        webElement2.click();
+        //driver.quit();
+
+} }
