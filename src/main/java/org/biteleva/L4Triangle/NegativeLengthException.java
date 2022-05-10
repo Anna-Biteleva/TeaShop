799621b741fe97778c39333b12ee0fa99c604a41
anbi <biteleva.anna@gmail.com>
2022-05-10 19:14:55 +0300

    ДЗ-4 TriangleSquare

--- src/main/java/org/biteleva/L4Triangle/NegativeLengthException.java
+++ src/main/java/org/biteleva/L4Triangle/NegativeLengthException.java
@@ -1,6 +1,6 @@
 package org.biteleva.L4Triangle;
 
- class NegativeLengthException extends Exception {
+  class NegativeLengthException extends Exception {
     public NegativeLengthException (String str){
         super(str);
     }
